bf8eba0e4bdfdd75baa3ba5a520d1043fc185336
Fedya9 <grofed4@gmail.com>
2025-02-10 18:05:08 +0200

    Comment

---
+++ classes/Comment.py
@@ -0,0 +1,17 @@
+import pygame
+
+from constants import *
+from helpers import screen
+from helpers import read_comment_from_user
+
+class Comment:
+    def __init__(self, comment):
+        self.comment = comment
+
+    def display(self, index):
+        font = pygame.font.SysFont("Verdana", COMMENT_TEXT_SIZE)
+        text = font.render(self.comment, True, (0, 0, 0))
+        screen.blit(text, [FIRST_COMMENT_X_POS, FIRST_COMMENT_Y_POS + index *COMMENT_LINE_HEIGHT ])
+
+
+
