f90a12a760dace4df7f7734c0e0fb9dd140d77cc
Fedya9 <grofed4@gmail.com>
2025-02-10 18:10:01 +0200

    ffff

--- classes/Comment.py
+++ classes/Comment.py
@@ -4,6 +4,7 @@ from constants import *
 from helpers import screen
 from helpers import read_comment_from_user
 
+
 class Comment:
     def __init__(self, comment):
         self.comment = comment
@@ -11,7 +12,4 @@ class Comment:
     def display(self, index):
         font = pygame.font.SysFont("Verdana", COMMENT_TEXT_SIZE)
         text = font.render(self.comment, True, (0, 0, 0))
-        screen.blit(text, [FIRST_COMMENT_X_POS, FIRST_COMMENT_Y_POS + index *COMMENT_LINE_HEIGHT ])
-
-
-
+        screen.blit(text, [FIRST_COMMENT_X_POS, FIRST_COMMENT_Y_POS + index * COMMENT_LINE_HEIGHT])
